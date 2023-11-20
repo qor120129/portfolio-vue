73c952031c46d6ee443f69d26c318f930472c664
qor120129 <qor120129@gmail.com>
2023-11-20 15:56:29 +0900

    p태그 오류 수정

--- src/components/Headline.vue
+++ src/components/Headline.vue
@@ -5,10 +5,10 @@
       THE OPEN <br/>
       MOVIE DATABASE
     </h1>
-    <P>
+    <p>
       The OMDb API is a RESTful web service to obtain movie information, all content and images on the site are contributed and maintained by our users. <br/>
       If you find this service useful, please consider making a one-time donation or become a patron.
-    </P>
+    </p>
   </div>
 </template>
 
